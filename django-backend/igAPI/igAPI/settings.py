8a1541e27ae0fa288851dc7f4c256639f5f7f253
theSuperpostionCat <49542462+zycs19@users.noreply.github.com>
2021-10-26 16:21:14 -0700

    install apps

--- django-backend/igAPI/igAPI/settings.py
+++ django-backend/igAPI/igAPI/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'UploadNewPostApp',
+    'ViewAllPostApp'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'igAPI.urls'
