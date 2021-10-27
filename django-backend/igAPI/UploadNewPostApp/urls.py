fc0c3e6c0e51646b648b0a85615b1885d439e983
theSuperpostionCat <49542462+zycs19@users.noreply.github.com>
2021-10-26 23:23:00 -0700

    photo upload api

--- django-backend/igAPI/UploadNewPostApp/urls.py
+++ django-backend/igAPI/UploadNewPostApp/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from UploadNewPostApp import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns =[
     url(r'^posts/$', views.postApi),
-    url(r'^posts/([0-9]+)$', views.postApi)
-]
+    url(r'^posts/([0-9]+)$', views.postApi),
+    url(r'^SavePhoto$', views.SavePhoto)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
